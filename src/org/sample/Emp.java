0373344c8c1a36a92677449581e83706b4f23115
Latha1906L <llathashree32@gmail.com>
2022-05-09 11:09:57 +0530

    llll

--- src/org/sample/Emp.java
+++ src/org/sample/Emp.java
@@ -3,13 +3,11 @@ package org.sample;
 public class Emp {
   public static void main(String[] args)
 	{
-		System.out.println("First Change");
-<<<<<<< HEAD
+		
+
 		System.out.println("Emp-2 changes");
 		System.out.println("Second Change emp2");
-=======
-		System.out.println("Emp-1");
->>>>>>> d2679f94e24d0fb2e9147c1f3a235cb990707133
+
 	}
 
 }
