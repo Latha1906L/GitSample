afcda058556ca86ff498dc96abd200480ffeea81
Latha1906L <llathashree32@gmail.com>
2022-05-09 21:06:03 +0530

    AAAAAAA

--- src/org/sample/Emp.java
+++ src/org/sample/Emp.java
@@ -4,8 +4,7 @@ public class Emp {
   public static void main(String[] args)
 	{
 
-		System.out.println("First Change");
-		System.out.println("Emp-1");
+	
 		
 		System.out.println("Third Change");
 		System.out.println("Emp-3");
